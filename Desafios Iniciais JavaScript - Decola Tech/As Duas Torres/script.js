d7c93b62060a66df0dacfaf1e05383a252900074
betafontes <robertafontesds@gmail.com>
2022-06-12 10:40:34 -0300

    desafios iniciais

--- Desafios Iniciais JavaScript - Decola Tech/As Duas Torres/script.js
+++ Desafios Iniciais JavaScript - Decola Tech/As Duas Torres/script.js
@@ -1,5 +1,3 @@
-// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
-// Abaixo segue um exemplo de código que você pode ou não utilizar
 
 let input = gets().split(" ")
 let distancia, dPSauron, dPSaruman; 
@@ -8,7 +6,6 @@ distancia =  parseInt(input[0]);
 diamPSauron =  parseInt(input[1]); 
 diamPSaruman =  parseInt(input[2]); 
 
-//TODO: Complete os espaços em branco com uma possível solução para o desafio
 
 let resultado = distancia / (diamPSauron + diamPSaruman ); 
 print(resultado.toFixed(2));
